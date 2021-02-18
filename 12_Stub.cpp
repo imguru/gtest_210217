a9a2e284c7a64139c97f22dea390d97dcf6064ec
Chansik Yun <chansigi@gmail.com>
2021-02-18 07:48:20 +0000

    .

--- 12_Stub.cpp
+++ 12_Stub.cpp
@@ -1,6 +1,5 @@
 
 // 12_Stub.cpp
-//  Test Stub Pattern
 
 // SUT
 #include <iostream>
@@ -54,12 +53,36 @@ public:
 
 // Player의 Move를 수행했을 때, Network 연결이 없을 경우, 예외가 제대로 발생하는지 여부를 검증하고 싶다.
 
+//  Test Stub Pattern
+//  의도: '다른 컴포넌트로부터의 간접 입력'에 의존하는 로직을 독립적으로 검증하고 싶다.
+//  방법: 실제 의존하는 객체를 테스트용 객체로 교체해서, SUT가 테스트하는데 필요한 결과를 보내도록 한다.
+//   => "특수한 상황"을 시뮬레이션 하고 싶다.
+//     : 네트워크 오류, 시간 등의 제어가 불가능한 환경을 제어할 수 있다.
+
+class StubConnection : public IConnection {
+public:
+	void Move(int x, int y) {
+		throw NetworkException();
+	}
+	virtual void Attack() {
+	}
+};
+
+
+TEST(PlayerTest, Move_TestDouble) {
+	StubConnection conn;
+	Player player(&conn);
+
+	EXPECT_THROW(player.Move(10, 20), NetworkException);
+}
+#if 0
 TEST(PlayerTest, Move) {
 	TCPConnection conn;
 	Player player(&conn);
 
 	EXPECT_THROW(player.Move(10, 20), NetworkException);
 }
+#endif
 
 
 
