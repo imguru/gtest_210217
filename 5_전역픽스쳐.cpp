64e301390afdd33ee7ddfa43ed69bae212d4b65f
Chansik Yun <chansigi@gmail.com>
2021-02-18 00:20:16 +0000

    .

--- 5_전역픽스쳐.cpp
+++ 5_전역픽스쳐.cpp
@@ -16,6 +16,7 @@ TEST(SampleTest, goo) {}
 TEST(MyTest, foo) {}
 TEST(MyTest, goo) {}
 
+// 주의사항: Google Test Framework에 등록되는 모든 객체는 반드시 힙에 생성되어야 한다.
 class MyTestEnvironment : public testing::Environment {
 public:
 	void SetUp() override {
@@ -29,11 +30,21 @@ public:
 
 //  전역 픽스쳐를 설치하는 방법 2가지
 //  1) 전역 변수 - libgtest에 main을 포함한 경우
-
-testing::Environment* env = testing::AddGlobalTestEnvironment(new MyTestEnvironment); 
-
+//   : main 함수가 호출되기 이전에 전역 변수에 대한 초기화가 되는 것을 이용한다.
+// testing::Environment* env = testing::AddGlobalTestEnvironment(new MyTestEnvironment); 
 
 //  2) main 함수 - libgtest에 main을 포함하지 않은 경우
+int main(int argc, char** argv) {
+	testing::InitGoogleTest(&argc, argv);
+	//----
+	testing::AddGlobalTestEnvironment(new MyTestEnvironment);
+	//----
+
+	return RUN_ALL_TESTS();
+}
+
+
+
 
 
 
