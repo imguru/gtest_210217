f12f629f7bd6567d873d6f0af1b0987230f88ba3
Chansik Yun <chansigi@gmail.com>
2021-02-18 02:46:55 +0000

    .

---
+++ 7_비기능테스트2.cpp
@@ -0,0 +1,51 @@
+#include <gtest/gtest.h>
+
+// 1. 제품 코드의 Image 객체에 대한 new / delete 재정의 되어야 합니다.
+// 2. 테스트 코드 에서만 재정의된 new / delete를 이용합니다.
+
+class Image {
+public:
+	static int allocCount;
+
+	// Image* p = new Image;
+	void* operator new(size_t size) {
+		++allocCount;
+		return malloc(size);
+	}
+
+	// delete p;
+	void operator delete(void* p, size_t) {
+		--allocCount;
+		free(p);
+	}
+};
+
+int Image::allocCount = 0;
+
+
+void Draw() {
+	Image* p1 = new Image;
+	Image* p2 = new Image;
+	Image* p3 = new Image;
+	Image* p4 = new Image;
+
+	delete p1;
+	delete p2;
+	delete p3;
+	delete p4;
+}
+
+TEST(ImageTest, Draw) {
+	int alloc = Image::allocCount;
+	Draw();
+	int diff = Image::allocCount - alloc;
+	
+	EXPECT_EQ(diff, 0) << diff << " object(s) leaks!";
+}
+
+
+
+
+
+
+
