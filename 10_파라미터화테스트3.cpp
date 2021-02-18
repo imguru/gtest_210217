88c83f3dc92b7fd151202151b9159cc1bc4c1473
Chansik Yun <chansigi@gmail.com>
2021-02-18 05:45:39 +0000

    .

--- 10_파라미터화테스트3.cpp
+++ 10_파라미터화테스트3.cpp
@@ -3,7 +3,28 @@
 
 enum Color { BLACK, GRAY, WHITE };
 const char* animals[] = { "cat", "dog" };
+// int ages[] = { 10, 20, 30, 40 };
 
+class AnimalTest : public testing::TestWithParam<std::tuple<const char*, Color, int>> {
+};
+
+using testing::Combine;
+using testing::ValuesIn;
+using testing::Values;
+using testing::Range;
+
+INSTANTIATE_TEST_SUITE_P(inputValues, AnimalTest, 
+	Combine(ValuesIn(animals), Values(BLACK, GRAY, WHITE), Range(1, 20, 5)));
+	// Combine(ValuesIn(animals), Values(BLACK, GRAY, WHITE), ValuesIn(ages)));
+
+TEST_P(AnimalTest, AnimalLooks) {
+	const char* animal = std::get<0>(GetParam());
+	Color color = std::get<1>(GetParam());
+	int age = std::get<2>(GetParam());
+
+	printf("%s - %d - %d\n", animal, color, age);
+}
+#if 0
 // Input Data
 //  1. "cat" Black
 //  2. "cat" GRAY
@@ -28,3 +49,4 @@ TEST_P(AnimalTest, AnimalLooks) {
 
 	printf("%s - %d\n", animal, color);
 }
+#endif
