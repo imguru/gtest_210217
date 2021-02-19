1fe83a4ef59431a1acc5a075924b4f09f5d7ceff
Chansik Yun <chansigi@gmail.com>
2021-02-19 02:25:15 +0000

    .

--- 16_Mocking.cpp
+++ 16_Mocking.cpp
@@ -11,3 +11,43 @@ struct MP3 {
 	virtual std::string GetName() const = 0;
 	virtual void Foo() noexcept = 0;
 };
+
+// Mocking
+//  - MOCK_METHOD(반환타입, 메소드이름, (인자), (한정자));
+//  한정자
+//   1. override  - virtual 함수(옵션) - 권장
+//   2. const - 필수
+//   3. noexcept - 필수
+//   4. stdcall - Windows Platform에서 주로 사용합니다.
+
+class MockMP3 : public MP3 {
+public:
+	MOCK_METHOD(std::string, GetName, (), (const, override));
+	MOCK_METHOD(void, Foo, (), (noexcept, override));
+
+	// void Play() override {} 
+	MOCK_METHOD(void, Play, (), (override));
+	MOCK_METHOD(void, Stop, (int n), (override));
+};
+
+void Sample(MP3* mp3) {
+	mp3->Play();
+	mp3->Stop(10);
+}
+
+TEST(MP3Test, Sample) {
+	MockMP3 mock;
+
+	EXPECT_CALL(mock, Play());
+	EXPECT_CALL(mock, Stop(10));
+
+	Sample(&mock);
+}
+
+
+
+
+
+
+
+
