3ded5daaa35c4ce61a28ff55965fc53e577c801c
Chansik Yun <chansigi@gmail.com>
2021-02-19 02:48:29 +0000

    .

--- 16_Mocking2.cpp
+++ 16_Mocking2.cpp
@@ -9,14 +9,25 @@ class Calc {
 public:
 	virtual ~Calc() {}  // 가상 소멸자는 상속을 기반으로 할때 반드시 필요합니다.
 
-	virtual int Add(Element x) {} // 행위 검증이 필요하지 않습니다.
-	virtual int Add(int times, Element x) {} // 행위 검증 필요합니다.
+	// 행위 검증이 필요하지 않습니다.
+	virtual int Add(Element x) {
+		printf("Calc.Add(Element)\n");
+	}
+	virtual int Add(int times, Element x) {
+		printf("Calc.Add(int, Element)\n");
+	}
+
+	// 행위 검증 필요합니다.
 };
 
 // 클래스를 상속할 수 있다면, 모의 객체로 만드는 것이 가능합니다.
 class MockCalc : public Calc {
 public:
+	// 더 이상 기존 함수는 호출되지 않습니다.
 	MOCK_METHOD(int, Add, (int times, Element x), (override));
+
+	// 함수가 가려지는 문제가 발생할 경우, 아래의 코드가 필요합니다.
+	using Calc::Add;
 };
 
 void Sample(Calc* p) {
@@ -33,7 +44,11 @@ TEST(MockCalc, Add) {
 
 	EXPECT_CALL(mock, Add(10, Element()));
 	
-	Sample(&mock);
+	// MockCalc가 Add라는 구현이 감추어지는 현상이 발생한다.
+	mock.Add(Element());
+	mock.Add(10, Element());
+
+	// Sample(&mock);
 }
 
 
